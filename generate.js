a3ded60a0b9ce2fa835a5c235d0026e658085681
PiciAkk <marci@dbx.hu>
2021-07-24 21:22:14 +0200

    main code uploaded

---
+++ generate.js
@@ -0,0 +1,14 @@
+const readline = require('readline-sync');
+const commandLineArguments = process.argv.slice(2);
+if (commandLineArguments.length == 1) {
+  var inputURL = commandLineArguments[0];
+} else if (commandLineArguments.length == 0) {
+  console.log("Can't read command-line arguments...")
+  var inputURL = readline.question("Enter input URL: ")
+} else {
+  console.log("Error: Too many command-line arguments...")
+  process.exit()
+}
+encodedURL = encodeURI(inputURL);
+outputURL = ("https://l.messenger.com/l.php?u=" + encodedURL)
+console.log(outputURL)
